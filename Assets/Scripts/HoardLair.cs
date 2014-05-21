15ac179029e2034ef8ea30644b3ac636c8126d2b
LochNeffMonster <atneff@ucsc.edu>
2014-05-20 22:59:03 -0700

    added a trigger volume where you can collect things

---
+++ Assets/Scripts/HoardLair.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HoardLair : MonoBehaviour {
+    // list of hoarded items
+    // public just for testing purposes
+    public List<Valuable> _hoardedItems = null;
+    // current value total
+    private float _totalValue = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        _hoardedItems = new List<Valuable>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnTriggerEnter(Collider other) {
+        if(other.GetComponent<Valuable>() != null){
+            Valuable valuableComponent = other.GetComponent<Valuable>();
+            _hoardedItems.Add(valuableComponent);
+            _totalValue += valuableComponent.getValue();
+            Debug.Log("Current Hoard value = " + _totalValue);
+        }   
+    }
+    
+    void OnTriggerExit(Collider other) {
+        if(other.GetComponent<Valuable>() != null){
+            Valuable valuableComponent = other.GetComponent<Valuable>();
+            _hoardedItems.Add(valuableComponent);
+            _totalValue -= valuableComponent.getValue();
+            Debug.Log("Current Hoard value = " + _totalValue);
+        } 
+        Debug.Log("Current Hoard value = " + _totalValue);
+    }
+    
+    public float getTotal(){
+        return _totalValue;
+    }
+    public List<Valuable> getValuables(){
+        return _hoardedItems;
+    }
+}
