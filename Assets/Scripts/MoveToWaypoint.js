4effe1dc0f27f1bc2a17923d9befd541b9f6a5d9
Paradoxium <anmcmill@ucsc.edu>
2014-05-24 17:03:37 -0700

    Fixed waypoints so that it uses the navmesh
    npc uses navmesh to go between waypoints. Can navigate through and
    around objects and buildings that have been added to the navmesh

--- Assets/Scripts/MoveToWaypoint.js
+++ Assets/Scripts/MoveToWaypoint.js
@@ -10,12 +10,14 @@ var waypoints : Transform[];
 private var currentWaypoint : Transform;
 private var currentIndex : int;
 
+private var agent: NavMeshAgent;
 //NPC Movement
-var moveSpeed : float = 10.0;
+//var moveSpeed : float = 10.0;
 var minDistance : float = 2.0;
 
 //initialization
 function Start () {
+	agent = GetComponent.<NavMeshAgent>();
 	currentWaypoint = waypoints[0];
 	currentIndex = 0;
 }
@@ -25,6 +27,7 @@ function Update () {
 	if(Vector3.Distance(currentWaypoint.transform.position, transform.position) < minDistance)
 	{
 		currentIndex++;
+		//resests after cycling though all waypoints.
 		if(currentIndex > waypoints.Length - 1)
 		{
 			currentIndex = 0;
@@ -35,10 +38,11 @@ function Update () {
 
 function MoveTowardsWaypoint() : void
 {
-	var direction : Vector3 = currentWaypoint.transform.position - transform.position;
-	var moveVector : Vector3 = direction.normalized * moveSpeed * Time.deltaTime;
-	transform.position += moveVector;
+	//var direction : Vector3 = currentWaypoint.transform.position - transform.position;
+	//var moveVector : Vector3 = direction.normalized * moveSpeed * Time.deltaTime;
+	//transform.position += moveVector;
+	agent.SetDestination(currentWaypoint.transform.position);
 	
 	//smoothly rotate towards target
-	transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 4 * Time.deltaTime);
+	//transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 4 * Time.deltaTime);
 }
