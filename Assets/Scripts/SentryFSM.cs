233402886b20cdfdfe3ee672baccccc429b6fd69
Tyler Austin Gatta <tylergatta@gmail.com>
2014-05-25 17:31:52 -0700

    [New Scene] Sentry animations test
    This scene has an animation FSM working with our sentry model. Use "F"
    to make him walk forward. Combining this with Andy's AI shouldn't be too
    hard -- I can do the implementation once his AI is ready.

---
+++ Assets/Scripts/SentryFSM.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SentryFSM : MonoBehaviour {
+	Animator anim;
+	
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+		
+	void Update ()
+	{
+
+		Debug.DrawLine(transform.position, Vector3.forward * Time.deltaTime, Color.red);
+		//Debug.DrawLine (Transform.position, Vector3.forward , Color.cyan);
+		if (Input.GetKey ("f")) {
+
+			transform.Translate (Vector3.forward * Time.deltaTime);
+			anim.SetBool("Walking", true);
+		}else{
+			anim.SetBool("Walking", false);
+		}
+	}
+}
